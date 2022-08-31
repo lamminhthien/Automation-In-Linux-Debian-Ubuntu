5d4265de2c45d0887809bc50e68a3fce5e1b1288
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-08-31 19:33:33 +0700

    add post-installation_popos

---
+++ post-installation-popos.sh
@@ -0,0 +1,15 @@
+# Install Local Site to Host Wordpress
+wget https://cdn.localwp.com/releases-stable/6.4.2+6012/local-6.4.2-linux.deb
+sudo dpkg -i local-6.4.2-linux.deb
+# Install NodeJS
+cd ~
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+sudo apt -y install nodejs
+## Install Postgres 13
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install postgresql
+# Install VMWare Tool 
+sudo apt-get update
+sudo apt-get install open-vm-tools-desktop
