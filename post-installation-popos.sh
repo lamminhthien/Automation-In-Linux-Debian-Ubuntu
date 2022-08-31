dac10d7bb5acbbbedf628b53aacadac0145bb568
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-08-31 19:58:07 +0700

    add usr bin bash

--- post-installation-popos.sh
+++ post-installation-popos.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 # Fix borken package before install anything
 sudo apt --fix-broken install -y
 # Install Local Site to Host Wordpress
@@ -18,5 +19,47 @@ sudo apt-get install open-vm-tools-desktop -y
 # Install Git
 sudo apt-get install git -y
 # Install DBeaver for manager database
-flatpak install flathub io.dbeaver.DBeaverCommunity
-
+flatpak install -y flathub io.dbeaver.DBeaverCommunity
+# Install VSCode
+sudo apt-get install wget gpg -y
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y
+sudo apt update
+sudo apt install code -y
+# Disable Animation in gnome desktop
+gsettings set org.gnome.desktop.interface enable-animations false
+# Install Yarn
+sudo npm install -g yarn
+# Install Ibus bamboo
+sudo add-apt-repository -y ppa:bamboo-engine/ibus-bamboo
+sudo apt-get update -y
+sudo apt-get install -y ibus ibus-bamboo --install-recommends
+ibus restart
+# Install Shutter for support ruler pixel in code front end
+sudo apt-get install -y shutter
+# Install Best Color picker tool call gpick
+sudo apt-get -y install gpick
+# Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+# Install Dolphin File Manager
+sudo apt install -y dolphin
+# Set Dolphin as default file manager
+xdg-mime default org.kde.dolphin.desktop inode/directory
+# Install GIMP 
+flatpak install -y flathub org.gimp.GIMP
+# Install Inkscape
+flatpak install -y flathub org.inkscape.Inkscape
+# Install Figma For Linux
+flatpak install -y flathub io.github.Figma_Linux.figma_linux
+# Install Github Desktop
+flatpak install -y flathub io.github.shiftey.Desktop
+# Install VLC
+flatpak install -y flathub org.videolan.VLC
+# Install WPS Office
+flatpak install -y flathub com.wps.Office
+# Install Lutris
+flatpak install -y flathub net.lutris.Lutris
