87c436fdc4fe981b2fbc197bcf7f8db133926044
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-09-01 07:24:36 +0700

    add readme, add script to create hotket

---
+++ setup_hotkey_ubuntu_desktop.sh
@@ -0,0 +1,17 @@
+# Learn more from this askubuntu.com https://askubuntu.com/questions/597395/how-to-set-custom-keyboard-shortcuts-from-terminal
+# Super key:                 <Super>
+# Control key:               <Primary> or <Control>
+# Alt key:                   <Alt>
+# Shift key:                 <Shift>
+# numbers:                   1 (just the number)
+# Spacebar:                  space
+# Slash key:                 slash
+# Asterisk key:              asterisk (so it would need `<Shift>` as well)
+# Ampersand key:             ampersand (so it would need <Shift> as well)
+
+# a few numpad keys:
+# Numpad divide key (`/`):   KP_Divide
+# Numpad multiply (Asterisk):KP_Multiply
+# Numpad number key(s):      KP_1
+# Numpad `-`:                KP_Subtract
+python3 /path/to/script.py 'open file explorer' 'dolphin ~/' '<Super>E'
